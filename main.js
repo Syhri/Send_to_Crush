54c4e178b089e5615323e9811526b4cd4bcb0862
Syhri <153348201+Syhri@users.noreply.github.com>
2024-09-07 21:02:15 +0800

    done

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
+const audio = document.querySelector(".audio")
+const typing = document.querySelector(".typed-out")
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML =
-    "You got Rickrolled 😘 (FOLLOW ME @kristel_tech or 7 years bad luck)";
-  gif.src =
-    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  question.innerHTML = "I love you toooo, hmmm jadi sayang dehhh 😍";
+  gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmphaTlsdmQyZ3l3M2h4Mms1dTZqc3FsMWNueGx0ajJ6YXluN3BuOSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MT6LcGLC1qJuYjn3ZE/giphy.gif";
+  noBtn.style.pointerEvents = "none";
+  typing.classList.remove("hide");
+  audio.play(); 
 });
 
 // Make the No button move randomly on hover
-noBtn.addEventListener("mouseover", () => {
+noBtn.addEventListener("click", () => {
   const wrapper = document.querySelector(".wrapper");
   const wrapperRect = wrapper.getBoundingClientRect();
   const noBtnRect = noBtn.getBoundingClientRect();
